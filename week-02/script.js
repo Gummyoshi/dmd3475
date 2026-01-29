5fc37bdd03dd2d4c37500bbb65ebb343de825915
Connor Lafferty <connor.lafferty@uconn.edu>
2026-01-29 11:51:20 -0500

    Week 2 in class examples

---
+++ week-02/script.js
@@ -0,0 +1,28 @@
+var week = 2;
+
+console.log("Hello, this is week ${week}");
+
+//alert("Hello, this is really week 2");
+
+console.log("Today is Thursday");
+
+var today = "thursday";
+//console.log(today);
+var tomorrow = "friday";
+
+//console.log("Today is Thursday");
+//console.log("Tomorrow is Friday");
+
+var greeting = "Today is " + today + ".";
+
+console.log(greeting);
+
+var nextWeek = week + 1;
+
+console.log("Next week will be week " + nextWeek);
+
+//var myName = "Connor";
+
+var myName = prompt("What is your name?");
+
+alert("My name is " + myName);
